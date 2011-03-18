c5630c86a2f484cf2ccdba38106746410c9cb686
Adam Fortuna <adam@fortuna.name>
2011-03-18 02:32:15 -0400

    Updated to use goalie for errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,4 @@ class ApplicationController < ActionController::Base
       redirect_to root_path, :error => "Unable to access that page."
     end
   end
-  
-  def render_optional_error_file(status_code)
-    status = interpret_status(status_code)
-    code   = status[0, 3] # "404 Not Found"
-    
-    respond_to do |wants|
-      wants.html { render :template => "errors/#{code}", :status => status }
-      wants.all { head status }
-    end
-  end
 end
