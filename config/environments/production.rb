1414efa769d899088d54ea7e543257bae25c749c
Adam Fortuna <adam@fortuna.name>
2011-03-24 17:44:43 -0400

    updated mailer to start sending them out again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,7 @@ Snaps::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => "lineofthought.com" }
+  
 end
