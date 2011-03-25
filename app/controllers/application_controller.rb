8a4d7da3fca07658a2aeee5490865aa860184f1d
Adam Fortuna <adam@fortuna.name>
2011-03-25 01:51:59 -0400

    Added redirect if domain isn't the right one

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_filter :ensure_domain
   protect_from_forgery
   include FastSessions
-
   
   protected  
   
   def require_admin!
     if !logged_in? || !current_user.admin?
       redirect_to root_path, :error => "Unable to access that page."
+    end
+  end
+  
+  # Disable SSL in Development and Test mode
+  def ssl_required?
+    !["development", "test"].include?(Rails.env)
+  end
+
+  def ensure_domain
+    if request.env['HTTP_HOST'] != Settings.root_domain
+      # HTTP 301 is a "permanent" redirect
+      redirect_to (Settings.root_url + request.env['PATH_INFO']), :status => 301
     end
   end
 end
