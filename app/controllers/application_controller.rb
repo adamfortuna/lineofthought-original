7a407a0ffca03d2cffaab57e47201b469f5f6f34
Adam Fortuna <adam@fortuna.name>
2011-03-27 17:42:22 -0400

    ssl fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,14 +22,16 @@ class ApplicationController < ActionController::Base
     # If not on a secure page, make sure the scheme is http
     puts "Server Name: #{request.env['SERVER_NAME']}, Host Name: #{request.env['HTTP_HOST']}, request schema: #{current_handy_url.scheme}"
     puts "Current request: #{request.env['REQUEST_URI']}"
-    puts "\n\nEnv: #{request.env}\n\n"
+    # puts "\n\nEnv: #{request.env}\n\n"
+    
+    schema = request.env['HTTP_X_FORWARDED_PROTO'] || "http"
     if request.env['HTTP_HOST'] == Settings.root_domain_with_port
       puts "required schema: Non-SSL - #{Settings.default_schema}"
-      redirect_to (Settings.root_url + request.env['PATH_INFO']), :status => 301 unless current_handy_url.scheme == Settings.default_schema
+      redirect_to (Settings.root_url + request.env['PATH_INFO']), :status => 301 if schema != Settings.default_schema
     # If on a secure page, make sure the scheme is https
     elsif request.env['HTTP_HOST'] == Settings.ssl_root_domain_with_port
       puts "required schema: SSL - #{Settings.ssl_schema}"
-      redirect_to (Settings.ssl_root_url + request.env['PATH_INFO']), :status => 301 unless current_handy_url.scheme == Settings.ssl_schema
+      redirect_to (Settings.ssl_root_url + request.env['PATH_INFO']), :status => 301 if schema != Settings.ssl_schema
     # If not on a known host ["www.lineofthought.com", "lineofthought.com"], redirect to lineofthought.com
     else
       puts "required schema: any"
