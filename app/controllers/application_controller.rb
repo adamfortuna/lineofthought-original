2921eb27457183a4aaba9fb429d77636be0eac4e
Adam Fortuna <adam@fortuna.name>
2011-03-27 17:19:41 -0400

    fixes for ssl

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,11 +23,11 @@ class ApplicationController < ActionController::Base
     puts "Server Name: #{request.env['SERVER_NAME']}, Host Name: #{request.env['HTTP_HOST']}, request schema: #{current_handy_url.scheme}"
     if request.env['HTTP_HOST'] == Settings.root_domain_with_port
       puts "required schema: SSL - #{Settings.ssl_schema}"
-      redirect_to (Settings.root_url + request.env['PATH_INFO']), :status => 301 unless current_handy_url.scheme == Settings.ssl_schema
+      redirect_to (Settings.root_url + request.env['PATH_INFO']), :status => 301 unless current_handy_url.scheme == Settings.default_schema
     # If on a secure page, make sure the scheme is https
     elsif request.env['HTTP_HOST'] == Settings.ssl_root_domain_with_port
       puts "required schema: Non-SSL - #{Settings.default_schema}"
-      redirect_to (Settings.ssl_root_url + request.env['PATH_INFO']), :status => 301 unless current_handy_url.scheme == Settings.default_schema
+      redirect_to (Settings.ssl_root_url + request.env['PATH_INFO']), :status => 301 unless current_handy_url.scheme == Settings.ssl_schema
     # If not on a known host ["www.lineofthought.com", "lineofthought.com"], redirect to lineofthought.com
     else
       puts "required schema: any"
