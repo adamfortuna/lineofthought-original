7c26f13aca6bddd1f9b21699414322a0717eea28
Adam Fortuna <adam@fortuna.name>
2011-03-28 11:43:17 -0400

    Fixing some cache expirations

--- app/sweepers/site_sweeper.rb
+++ app/sweepers/site_sweeper.rb
@@ -1,22 +1,24 @@
 class SiteSweeper < ActionController::Caching::Sweeper
   observe Site
   
-  def after_update(widget)
-    expire_cache_for(widget)
+  def after_update(site)
+    expire_cache_for(site)
   end
 
-  def after_destroy(widget)
-    expire_cache_for(widget)
+  def after_destroy(site)
+    expire_cache_for(site)
   end
 
-  def after_create(widget)
-    expire_cache_for(widget)
+  def after_create(site)
+    expire_cache_for(site)
   end
 
   private
-  def expire_cache_for(widget)
-    expire_action(:controller => "sites", :action => "show", :id => widget.cached_slug)
-    expire_action(:controller => "sites", :action => "show", :id => widget.cached_slug, :logged_in => true)
-    expire_action(:controller => "sites", :action => "show", :id => widget.cached_slug, :logged_in => true, :claimed => true)
+  def expire_cache_for(site)
+    expire_action(:controller => "sites", :action => "show", :id => site.cached_slug)
+    expire_action(:controller => "sites", :action => "show", :id => site.cached_slug, :logged_in => true)
+    expire_action(:controller => "sites", :action => "show", :id => site.cached_slug, :logged_in => true, :claimed => true)
+    
+    expire_fragment('featured_sites') if site.featured?
   end
 end
--- app/sweepers/tool_sweeper.rb
+++ app/sweepers/tool_sweeper.rb
@@ -1,22 +1,23 @@
 class ToolSweeper < ActionController::Caching::Sweeper
   observe Tool
   
-  def after_update(widget)
-    expire_cache_for(widget)
+  def after_update(tool)
+    expire_cache_for(tool)
   end
 
-  def after_destroy(widget)
-    expire_cache_for(widget)
+  def after_destroy(tool)
+    expire_cache_for(tool)
   end
 
-  def after_create(widget)
-    expire_cache_for(widget)
+  def after_create(tool)
+    expire_cache_for(tool)
   end
 
   private
-  def expire_cache_for(widget)
-    expire_action(:controller => "tools", :action => "show", :id => widget.cached_slug)
-    expire_action(:controller => "tools", :action => "show", :id => widget.cached_slug, :logged_in => true)
-    expire_action(:controller => "tools", :action => "show", :id => widget.cached_slug, :logged_in => true, :claimed => true)
+  def expire_cache_for(tool)
+    expire_action(:controller => "tools", :action => "show", :id => tool.cached_slug)
+    expire_action(:controller => "tools", :action => "show", :id => tool.cached_slug, :logged_in => true)
+    expire_action(:controller => "tools", :action => "show", :id => tool.cached_slug, :logged_in => true, :claimed => true)
+    expire_fragment('featured_tools') if using.tool.featured?
   end
 end
