9b98217272a4577fe702d3fc9c65fb3194442ea2
Adam Fortuna <adam@fortuna.name>
2011-03-29 01:56:30 -0400

    Probaly shouldn't commit with stuff commented out

--- db/migrate/20110329052526_update_bookmarks_for_multiple_users.rb
+++ db/migrate/20110329052526_update_bookmarks_for_multiple_users.rb
@@ -1,16 +1,16 @@
 class UpdateBookmarksForMultipleUsers < ActiveRecord::Migration
   def self.up
-    # add_column :bookmarks, :user_bookmarks_count, :integer, :default => 0
-    # 
-    # create_table :user_bookmarks do |t|
-    #   t.datetime :created_at
-    #   t.integer :bookmark_id, :user_id
-    #   t.string :title
-    #   t.text :description
-    #   t.boolean :has_video, :default => false
-    #   t.boolean :has_presentation, :default => false
-    # end
-    # add_index :user_bookmarks, :bookmark_id
+    add_column :bookmarks, :user_bookmarks_count, :integer, :default => 0
+    
+    create_table :user_bookmarks do |t|
+      t.datetime :created_at
+      t.integer :bookmark_id, :user_id
+      t.string :title
+      t.text :description
+      t.boolean :has_video, :default => false
+      t.boolean :has_presentation, :default => false
+    end
+    add_index :user_bookmarks, :bookmark_id
     add_index :user_bookmarks, :user_id
     
     add_column :annotations, :user_bookmark_id, :integer
