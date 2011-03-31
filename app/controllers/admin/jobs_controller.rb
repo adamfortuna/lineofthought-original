26f0c47d37c6a42b055f84e78d78e6850f4429e0
Adam Fortuna <adam@fortuna.name>
2011-03-31 16:38:29 -0400

    Fix for admin

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,5 +1,6 @@
 class Admin::JobsController < Admin::BaseController
   def index
+    @user = current_user
     @delayed_jobs = Delayed::Job.all.paginate(:page => params[:page] || 1, :per_page => params[:per_page] || 25)
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < Admin::BaseController
   def index
+    @user = current_user
     @users = User.all.paginate(:page => params[:page] || 1, :per_page => params[:per_page] || 25)
   end
 end
