e560a9efb617c8861ffd8d849b0c9e1eb0558b3e
Adam Fortuna <adam@fortuna.name>
2011-04-05 18:00:18 -0400

    Fix for caching wrong thing for index page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  caches_action :index, :cache_path => Proc.new { |controller| controller.params.merge(:logged_in => logged_in? ) }, :expires_in => 15.minutes
+  caches_action :index, :cache_path => Proc.new { |controller| controller.params.merge(:logged_in => logged_in? ) }, :expires_in => 15.minutes, :layout => false
 
   # GET /
   def index
